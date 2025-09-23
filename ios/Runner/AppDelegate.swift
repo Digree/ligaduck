62f16fcbfe08ab0d5d1694d42d95bc6e181a2ae7
Digree <emadigregorio@gmail.com>
2025-09-23 18:12:48 +0200

    Aggiornato Flutter

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,14 @@ import Flutter
 
 @UIApplicationMain
 class AppDelegate: FlutterAppDelegate {
+  var flutterEngine = FlutterEngine(name: "MyFlutterEngine")
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    flutterEngine.run()
+    GeneratedPluginRegistrant.register(with: flutterEngine)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
--- ios/SceneDelegate.swift
+++ ios/SceneDelegate.swift
@@ -4,17 +4,14 @@ import Flutter
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
 
-  @available(iOS 13.0, *)
   func scene(
     _ scene: UIScene,
     willConnectTo session: UISceneSession,
     options connectionOptions: UIScene.ConnectionOptions
   ) {
-    guard let windowScene = scene as? UIWindowScene else { return }
+    guard let windowScene = (scene as? UIWindowScene) else { return }
     let window = UIWindow(windowScene: windowScene)
-    let flutterEngine = (UIApplication.shared.delegate as! AppDelegate).flutterEngine
-    let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
-    window.rootViewController = flutterViewController
+    window.rootViewController = FlutterViewController()
     self.window = window
     window.makeKeyAndVisible()
   }
